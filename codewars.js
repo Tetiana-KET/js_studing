e7a5a575589f027038ef8bf9bdff778a0eed7882
Tetiana-KET <belangelphone@gmail.com>
2022-09-17 17:07:04 +0300

    kata Head, Tail, Init and Last

--- codewars.js
+++ codewars.js
@@ -204,5 +204,39 @@ function nthFibo(n) {
   return fiboArr[n - 1]; 
 };*/
   
+//Your job is to implement these functions in your given language.
+//Make sure it doesn't edit the array; that would cause problems! Here's a cheat sheet:
+// Here's how I expect the functions to be called in your language:
 
+// head([1,2,3,4,5]); => 1
+// tail([1, 2, 3, 4, 5]); => [2, 3, 4, 5]
 
+function head (arr) {
+   let result = arr[0];
+  return result;
+};
+
+function tail (arr) {
+   
+  if (arr.length == 1) {
+    return [];
+  }
+  
+   return arr.slice(1);;
+};
+
+function init (arr) {
+  return arr.slice(0, -1)
+};
+
+function last(arr) {
+  return arr[arr.length -1]
+}
+
+function head(a) {return a[0]}
+
+function last(a) {return a[a.length - 1]}
+
+function init(a) {return a.slice(0, -1)}
+
+function tail(a) {return a.slice(1)}
