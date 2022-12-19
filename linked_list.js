6948240ac2e280bf565f9bb5f9881f776a701dc5
Tetiana-KET <belangelphone@gmail.com>
2022-12-19 10:05:32 +0200

    modified:   linked_list.js

--- linked_list.js
+++ linked_list.js
@@ -158,3 +158,37 @@ class LinkedList {
         }
     }
 }
+
+/** УДАЛИТЬ ОПРЕДЕЛЕННЫЙ ЭЛЕМЕНТ ИЗ СПИСКА
+ * Given a singly linked list of integers l and an integer k,
+ * remove all elements from list l that have a value equal to k.
+ *
+ * @param {List} l
+ * @param {Number} k
+ * @return {List}
+ *
+ * @example
+ * For l = [3, 1, 2, 3, 4, 5] and k = 3,
+ * the output should be [1, 2, 4, 5]
+ *
+ * Singly - linked lists are already defined using interface
+ * class ListNode {
+ *   constructor(x) {
+ *     this.value = x;
+ *     this.next = null;
+ *   }
+ * }
+ */
+
+function removeKFromList(l, k) {
+    let current = this.head;
+
+        while (current) {
+            if (current.value === k) {
+                current = current.next;
+            }
+            current = current.next;
+        }
+        return l;
+}
+console.log(removeKFromList([3, 1, 2, 3, 4, 5], 3));
